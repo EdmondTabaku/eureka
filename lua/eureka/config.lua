227fe6b1736c692f865adf42fdf0f5874e7d0edb
Dronakurl <konrad2710@gmail.com>
2023-11-13 23:55:40 +0100

    Simply and learn from the maintainer

--- lua/eureka/config.lua
+++ lua/eureka/config.lua
@@ -4,16 +4,16 @@ local default_config = {
     default_notes = {
         "gg - Go to file start",
     },
-    close_key = 'q',
+    close_key = "q",
 }
 
 local user_config = {}
 
 function M.set(config)
-    user_config = vim.tbl_deep_extend('force', {}, default_config, config)
+    user_config = vim.tbl_deep_extend("force", {}, default_config, config)
 end
 
-function M.get_default_notes()
+function M.get_notes()
     return user_config.default_notes
 end
 
--- lua/eureka/init.lua
+++ lua/eureka/init.lua
@@ -1,18 +1,23 @@
+-- NOTE: This file is called whenever require("eureka") is called. No need for a init.lua on the top level
 local config = require("eureka.config")
 local ui = require("eureka.ui")
 
-local function setup(user_config)
+local M = {}
+
+M._notes = {}
+
+M.setup = function(user_config)
     config.set(user_config)
 end
 
-local function show_notes()
-    local default_notes = config.get_default_notes()
+M.show_notes = function()
+    M._notes = {}
+    -- TODO: This creates a copy of the notes
+    for _, v in ipairs(config.get_notes()) do
+        table.insert(M._notes, v)
+    end
     local close_key = config.get_close_key()
-    ui.display(default_notes, close_key)
+    ui.display(M._notes, close_key)
 end
 
-return {
-    setup = setup,
-    show_notes = show_notes,
-}
-
+return M
